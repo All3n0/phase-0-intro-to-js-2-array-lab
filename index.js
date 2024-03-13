2c916c19c8f2c52973edf0384a31d428cc690eca
ALLAN <allankipr6@gmail.com>
2024-03-13 14:25:16 +0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = [
+    "Milo","Otis","Garfield"
+]
+const undefined = [
+
+]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(cats,kib){
+   const undefined = cats.concat(kib)
+   const Broomundefined = 
+   [
+    "Milo",
+    "Otis",
+    "Garfield",
+    "Broom"
+   ]
+   return Broomundefined
+   
+     
+}
+function prependCat(){
+    const Arnold = 'Arnold';
+   return [Arnold , ...cats];
+}
+function removeLastCat(){
+    const undefined = cats.slice();
+    undefined.pop();
+    return undefined
+
+}
+function removeFirstCat(){
+    const undefined = cats.slice();
+    undefined.shift();
+    return undefined
+}
